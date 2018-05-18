9558b2fcdc15d005d6d53b4307a42c422fecaf60
ArcticAvalanche <38980549+ArcticAvalanche@users.noreply.github.com>
2018-05-19 01:13:25 +0300

    Delete, rename and some fixes

--- avalanche-fm/MainWindow.Designer.cs
+++ avalanche-fm/MainWindow.Designer.cs
@@ -34,9 +34,16 @@
             this.buttonBack = new System.Windows.Forms.Button();
             this.txtPath = new System.Windows.Forms.TextBox();
             this.treeView = new System.Windows.Forms.TreeView();
-            this.listView = new System.Windows.Forms.ListView();
-            this.listViewImageList = new System.Windows.Forms.ImageList(this.components);
             this.treeViewImageList = new System.Windows.Forms.ImageList(this.components);
+            this.listView = new System.Windows.Forms.ListView();
+            this.listViewContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.stripCreateFolder = new System.Windows.Forms.ToolStripMenuItem();
+            this.stripCreateFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.stripCreateTxtFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.stripRefresh = new System.Windows.Forms.ToolStripMenuItem();
+            this.listViewImageList = new System.Windows.Forms.ImageList(this.components);
+            this.listViewContextMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonBack
@@ -77,12 +84,21 @@
             this.treeView.Size = new System.Drawing.Size(161, 397);
             this.treeView.TabIndex = 2;
             // 
+            // treeViewImageList
+            // 
+            this.treeViewImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("treeViewImageList.ImageStream")));
+            this.treeViewImageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.treeViewImageList.Images.SetKeyName(0, "hard-drive-disk-icon.png");
+            this.treeViewImageList.Images.SetKeyName(1, "agf_greene.png");
+            this.treeViewImageList.Images.SetKeyName(2, "Computer-256x256.png");
+            // 
             // listView
             // 
             this.listView.Activation = System.Windows.Forms.ItemActivation.TwoClick;
             this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.ContextMenuStrip = this.listViewContextMenu;
             this.listView.LargeImageList = this.listViewImageList;
             this.listView.Location = new System.Drawing.Point(179, 41);
             this.listView.MultiSelect = false;
@@ -92,20 +108,57 @@
             this.listView.UseCompatibleStateImageBehavior = false;
             this.listView.ItemActivate += new System.EventHandler(this.listView_ItemActivate);
             // 
+            // listViewContextMenu
+            // 
+            this.listViewContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stripCreateFolder,
+            this.stripCreateFile,
+            this.toolStripSeparator1,
+            this.stripRefresh});
+            this.listViewContextMenu.Name = "listViewContextMenu";
+            this.listViewContextMenu.Size = new System.Drawing.Size(181, 98);
+            this.listViewContextMenu.Opening += new System.ComponentModel.CancelEventHandler(this.listViewContextMenu_Opening);
+            // 
+            // stripCreateFolder
+            // 
+            this.stripCreateFolder.Name = "stripCreateFolder";
+            this.stripCreateFolder.Size = new System.Drawing.Size(180, 22);
+            this.stripCreateFolder.Text = "Create folder";
+            this.stripCreateFolder.Click += new System.EventHandler(this.stripCreateFolder_Click);
+            // 
+            // stripCreateFile
+            // 
+            this.stripCreateFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stripCreateTxtFile});
+            this.stripCreateFile.Name = "stripCreateFile";
+            this.stripCreateFile.Size = new System.Drawing.Size(180, 22);
+            this.stripCreateFile.Text = "Create file";
+            // 
+            // stripCreateTxtFile
+            // 
+            this.stripCreateTxtFile.Name = "stripCreateTxtFile";
+            this.stripCreateTxtFile.Size = new System.Drawing.Size(149, 22);
+            this.stripCreateTxtFile.Text = "Create text file";
+            this.stripCreateTxtFile.Click += new System.EventHandler(this.stripCreateTxtFile_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // stripRefresh
+            // 
+            this.stripRefresh.Name = "stripRefresh";
+            this.stripRefresh.Size = new System.Drawing.Size(180, 22);
+            this.stripRefresh.Text = "Refresh";
+            this.stripRefresh.Click += new System.EventHandler(this.stripRefresh_Click);
+            // 
             // listViewImageList
             // 
             this.listViewImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("listViewImageList.ImageStream")));
             this.listViewImageList.TransparentColor = System.Drawing.Color.Transparent;
             this.listViewImageList.Images.SetKeyName(0, "hard-drive-disk-icon.png");
             this.listViewImageList.Images.SetKeyName(1, "agf_greene.png");
-            // 
-            // treeViewImageList
-            // 
-            this.treeViewImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("treeViewImageList.ImageStream")));
-            this.treeViewImageList.TransparentColor = System.Drawing.Color.Transparent;
-            this.treeViewImageList.Images.SetKeyName(0, "hard-drive-disk-icon.png");
-            this.treeViewImageList.Images.SetKeyName(1, "agf_greene.png");
-            this.treeViewImageList.Images.SetKeyName(2, "Computer-256x256.png");
             // 
             // MainWindow
             // 
@@ -119,6 +172,7 @@
             this.Name = "MainWindow";
             this.Text = "Avalanche File Manager";
             this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.listViewContextMenu.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,6 +186,12 @@
         private System.Windows.Forms.ListView listView;
         private System.Windows.Forms.ImageList listViewImageList;
         private System.Windows.Forms.ImageList treeViewImageList;
+        private System.Windows.Forms.ContextMenuStrip listViewContextMenu;
+        private System.Windows.Forms.ToolStripMenuItem stripCreateFile;
+        private System.Windows.Forms.ToolStripMenuItem stripCreateFolder;
+        private System.Windows.Forms.ToolStripMenuItem stripCreateTxtFile;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem stripRefresh;
     }
 }
 
---
+++ avalanche-fm/MainWindow.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace avalanche_fm
+{
+    public partial class MainWindow : Form
+    {
+        #region Константы
+        const string errorMessageCaption = "Error";
+        const string standartFileName = "New file";
+        const string standartFolderName = "New folder";
+        #endregion
+
+        //???
+        List<string> specialNodes = new List<string>();
+
+        #region Конструктор
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            GoToDrivesDirectory();
+
+            //InitializeTreeView();
+        }
+
+        private void GoToDrivesDirectory()
+        {
+            listView.Items.Clear();
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (var drive in drives)
+            {
+                ListViewItem driveItem = new ListViewItem()
+                {
+                    Text = drive.Name,
+                    ImageIndex = 0
+                };
+
+                listView.Items.Add(driveItem);
+            }
+        }
+
+        private void GoToSubdirectory(string name)
+        {
+            string path = txtPath.Text;
+            if (path == "")
+            {
+                path = name;
+            }
+            else
+            {
+                string tempoPath = path;
+                if (tempoPath.Remove(0, tempoPath.Length - 1) != @"\") path += @"\" + name;
+                else path += name;
+            }
+
+            if (File.Exists(path))
+            {
+                Process.Start(path);
+            }
+            else
+            {
+                txtPath.Text = path;
+                GoToDirectory(txtPath.Text);
+            }
+        }
+
+        private void GoToDirectory(string path)
+        {
+            listView.Items.Clear();
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(path);
+
+                foreach (var dir in dirInfo.GetDirectories())
+                {
+                    ListViewItem dirItem = new ListViewItem()
+                    {
+                        Text = dir.Name,
+                        ImageIndex = 1,
+                        
+                    };
+
+                    listView.Items.Add(dirItem);
+                }
+
+                foreach (var file in dirInfo.GetFiles())
+                {
+                    listViewImageList.Images.Add(System.Drawing.Icon.ExtractAssociatedIcon(path + @"\" + file.Name));
+                    ListViewItem fileItem = new ListViewItem()
+                    {
+                        Text = file.Name,
+                        ImageIndex = listViewImageList.Images.Count - 1
+                    };
+
+                    listView.Items.Add(fileItem);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, errorMessageCaption);
+            }
+        }
+
+        private void GoToRootDirectory()
+        {
+            if (txtPath.Text != "") txtPath.Text = Path.GetDirectoryName(txtPath.Text);
+            if (txtPath.Text != "") GoToDirectory(txtPath.Text);
+            else GoToDrivesDirectory();
+        }
+
+        private void RefreshListView()
+        {
+            if (txtPath.Text != "") GoToDirectory(txtPath.Text);
+            else GoToDrivesDirectory();
+        }
+
+        private void InitializeTreeView()
+        {
+            specialNodes.Add("Desktop");
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (var drive in drives)
+            {
+                TreeNode driveNode = new TreeNode()
+                {
+                    Text = drive.Name,
+                    ImageIndex = 0,
+                    SelectedImageIndex = 0
+                };
+
+                treeView.Nodes.Add(driveNode);
+            }
+
+            try
+            {
+                foreach (TreeNode node in treeView.Nodes)
+                {
+                    if (!specialNodes.Contains(node.Text))
+                    {
+                        DirectoryInfo di = new DirectoryInfo(node.Text);
+                        DirectoryInfo[] directories = di.GetDirectories("*", SearchOption.AllDirectories);
+
+                        foreach (var dir in directories)
+                        {
+                            TreeNode dirNode = new TreeNode()
+                            {
+                                Text = dir.Name,
+                                ImageIndex = 1,
+                                SelectedImageIndex = 1
+                            };
+
+                            treeView.Nodes[0].Nodes.Add(dirNode);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, errorMessageCaption);
+            }
+        }
+
+        private void listView_ItemActivate(object sender, EventArgs e)
+        {
+            if (listView.FocusedItem != null)
+            {
+                GoToSubdirectory(listView.FocusedItem.Text);
+            }
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            GoToRootDirectory();
+        }
+
+        private void stripCreateTxtFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int i = 0;
+                while (File.Exists(txtPath.Text + @"\" + standartFileName + (i == 0 ? "" : i.ToString()) + ".txt")) i++;
+                string tempFileName = standartFileName + (i == 0 ? "" : i.ToString()) + ".txt";
+                using (StreamWriter createFile = File.CreateText(txtPath.Text + @"\" + tempFileName))
+                {
+                    
+                }
+                RefreshListView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, errorMessageCaption);
+            }
+        }
+
+        private void stripCreateFolder_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            while (Directory.Exists(txtPath.Text + @"\" + standartFolderName + (i == 0 ? "" : i.ToString()))) i++;
+            string tempFolderName = standartFolderName + (i == 0 ? "" : i.ToString());
+            try
+            {
+                Directory.CreateDirectory(txtPath.Text + @"\" + tempFolderName);
+                RefreshListView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, errorMessageCaption);
+            }
+        }
+
+        private void stripRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshListView();
+        }
+
+        private void listViewContextMenu_Opening(object sender, CancelEventArgs e)
+        {
+            ToolStripMenuItem stripDelete = new ToolStripMenuItem("Delete", null, stripDelete_Click);
+            ToolStripMenuItem stripRename = new ToolStripMenuItem("Rename", null, stripRename_Click);
+
+            ToolStripTextBox txtRename = new ToolStripTextBox
+            {
+                Text = standartFileName
+            };
+
+            stripRename.DropDownItems.Add(txtRename);
+
+            if (listView.SelectedItems.Count != 0)
+            {
+                if (listViewContextMenu.Items.Count < 5)
+                {
+                    listViewContextMenu.Items.Add(stripRename);
+                    listViewContextMenu.Items.Add(stripDelete);
+                }
+
+                if (txtPath.Text == "")
+                {
+                    listViewContextMenu.Items[5].Enabled = false;
+                    listViewContextMenu.Items[4].Enabled = false;
+                }
+                else
+                {
+                    listViewContextMenu.Items[5].Enabled = true;
+                    listViewContextMenu.Items[4].Enabled = true;
+                }
+            }
+            else
+            {
+                if (listViewContextMenu.Items.Count >= 5)
+                {
+                    listViewContextMenu.Items.Remove(listViewContextMenu.Items[5]);
+                    listViewContextMenu.Items.Remove(listViewContextMenu.Items[4]);
+                }
+            }
+        }
+
+        private void stripRename_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string oldPath = txtPath.Text + @"\" + listView.SelectedItems[0].Text;
+                string extension;
+                extension = Path.GetExtension(oldPath);
+                string newPath = txtPath.Text + @"\" +
+                    (sender as ToolStripMenuItem).DropDownItems[0].Text + extension;
+
+                if (File.Exists(oldPath))
+                {
+                    File.Move(oldPath, newPath);
+                }
+
+                if (Directory.Exists(oldPath))
+                {
+                    Directory.Move(oldPath, newPath);
+                }
+
+                (sender as ToolStripMenuItem).DropDownItems[0].Text = standartFileName;
+
+                RefreshListView();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void stripDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = txtPath.Text + @"\" + listView.SelectedItems[0].Text;
+
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+
+                if (Directory.Exists(path))
+                {
+                    Directory.Delete(path);
+                }
+
+                RefreshListView();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
